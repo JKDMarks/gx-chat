a5978e59ee450fdb4452a362aeafc8743e42a331
Jeffrey Marks <jkdmarks@gmail.com>
2023-08-22 12:34:07 -0400

    Remove messageLength

--- src/App.js
+++ src/App.js
@@ -1,48 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
-class post{
-	constructor(m,n,c){
-		this.message = m;
-		this.name = n;
-		this.color = c;
-	};
-}
-
 function App() {
     const [posts, setPosts] = useState([]);
-    const [message, setNewMessage] = useState("");
-	const [name, setNewName] = useState("");
-	const [color, setNewColor] = useState("#0fa000");
-	const [messageLength, setMessageLength] = useState(0);
-	
-	
+    const [message, setMessage] = useState("");
+    const [name, setName] = useState("");
+    const [color, setColor] = useState("#0fa000");
 
-    
-	const handleNewMsgChange = (e) => {
-        setNewMessage(e.target.value);
-		setMessageLength(e.target.value.length);
+    const handleNewMsgChange = (e) => {
+        setMessage(e.target.value);
     };
-	
-	const handleNameChange = (e) => {
-		setNewName(e.target.value);	
-	}; 
-	
-	const handleColorChange = (e) => {
-		setNewColor(e.target.value);
-	};
-	
-	
+
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    };
+
+    const handleColorChange = (e) => {
+        setColor(e.target.value);
+    };
+
     const handleNewMsgSubmit = (e) => {
         e.preventDefault();
-        setPosts([...posts, new post(message,name,color)]);
-        setNewMessage("");
-		setMessageLength(0);
+        setPosts([...posts, { message, name, color }]);
+        setMessage("");
     };
 
-	
-	
     return (
         <div className="App">
             <img src="/gx-logo.jpg" style={{ position: "sticky", top: "5px", left: "5px", height: "50px" }} />
@@ -50,19 +33,21 @@ function App() {
                 <div id="chatbox">
                     {posts.map((msg) => (
                         <div className="post">
-							<p className="name" style={{color:msg.color}}>{msg.name}</p>
-							<p className="message">{msg.message}</p>
-						</div>
+                            <p className="name" style={{ color: msg.color }}>
+                                {msg.name}
+                            </p>
+                            <p className="message">{msg.message}</p>
+                        </div>
                     ))}
                 </div>
                 <div id="new-message-box">
                     <form onSubmit={handleNewMsgSubmit}>
                         <input type="text" maxlength="1000" value={message} onChange={handleNewMsgChange}></input>
-						<input type="text" maxlength="10" value={name} onChange={handleNameChange}></input>
-						<input type="text" maxlength="7" value={color} onChange={handleColorChange}></input>
+                        <input type="text" maxlength="10" value={name} onChange={handleNameChange}></input>
+                        <input type="text" maxlength="7" value={color} onChange={handleColorChange}></input>
                         <button type="submit">Post!</button>
                     </form>
-					<p id="charCounter">{messageLength}/1000</p>
+                    <p id="charCounter">{message.length}/1000</p>
                 </div>
             </div>
         </div>
